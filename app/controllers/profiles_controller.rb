2a5941d9d8f3afd42de3980557f7af400a4c2912
vasaviNarayana <nagavasavi1999@gmail.com>
2023-12-18 17:20:02 -0500

    removed unwanted files

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,6 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
 
-  # GET /profiles
-  # def index
-  #   @profiles = Profile.all
-  # end
-
   # GET /profiles/1
   def show
     @customer_user = Customerinfo.find_by(username: session[:customer_username])
@@ -15,33 +10,9 @@ class ProfilesController < ApplicationController
     @chef_reviews = @chef.chef_reviews
   end
 
-  # GET /profiles/new
-  # def new
-  #   @profile = Profile.new
-  # end
-
-  # GET /profiles/1/edit
-  # def edit
-  # end
-
-  # POST /profiles
-  # def create
-  # end
-
-  # DELETE /profiles/1
-  # def destroy
-  #   @profile.destroy
-  #   redirect_to profiles_url, notice: 'Profile was successfully destroyed.'
-  # end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_profile
       @profile = Chefinfo.find(params[:id])
     end
-
-    # Only allow a trusted parameter "white list" through.
-    # def profile_params
-    #   params[:profile]
-    # end
 end
